f43ebb448688bcd49c7425e98b09126bfe6541d4
ankitjain0748 <ankitjain0748@gmail.com>
2025-11-02 23:58:18 +0530

    amam

--- src/Admin/Apis/Api.js
+++ src/Admin/Apis/Api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL =  process.env.REACT_BASE_URL  || "http://localhost:5000/api";
+const API_URL =  process.env.REACT_BASE_URL  || "https://api.cadmaxpro.com/api";
 
 // || "http://localhost:5000/api"
 function getToken() {
