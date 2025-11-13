1d34c77f06100d0871796ffc365b35c18c2d2654
ankitjain0748 <ankitjain0748@gmail.com>
2025-11-13 19:20:15 +0530

    manage desgin

--- src/Home/ReadyBring.jsx
+++ src/Home/ReadyBring.jsx
@@ -6,7 +6,7 @@ export default function Readybring() {
     <section className="relative h-[300px] md:h-[350px] lg:h-[450px] flex items-center justify-center">
       {/* Overlay */}
       <img src={"https://cadmaxpro-buket.s3.ap-south-1.amazonaws.com/assets/home/insur.png"} alt="Interior" className=" w-full h-full object-cover" />
-      {/* <div className="absolute inset-0 bg-black bg-opacity-60"></div> */}
+      {/* <div className="absolute inset-0 bg-black bg-opacity-40"></div> */}
 
       {/* Content */}
       <div className="h-full flex items-center justify-center flex-col absolute left-[0] right-[0] top-[0] bottto-[0] z-10 text-center px-4 md:px-8">
